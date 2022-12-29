d938bd9d9c9dbd214c7f9eaee63322a23f54d46b
Dennis Chung <stvreumi.cs04@g2.nctu.edu.tw>
2022-12-29 20:37:54 +0800

    chore: :wrench: Update site description

--- site.config.ts
+++ site.config.ts
@@ -9,12 +9,12 @@ export default siteConfig({
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Next.js Notion Starter Kit',
-  domain: 'nextjs-notion-starter-kit.transitivebullsh.it',
+  name: 'Dennis Chung',
+  domain: 'personal-fita80qhq-stvreumi.vercel.app',
   author: 'Dennis Chung',
 
   // open graph metadata (optional)
-  description: 'Example Next.js Notion Starter Kit Site',
+  description: 'Dennis Chung\' notes(experimental)',
 
   // social usernames (optional)
   // twitter: 'transitive_bs',
